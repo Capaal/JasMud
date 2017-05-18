8aa9898ef35e3ebc1745b5e63ee705aaf543eaae
Xin Xin Li <mysanthil@gmail.com>
2017-05-17 20:44:30 -0700

    Added bag.

---
+++ src/items/Bag.java
@@ -0,0 +1,64 @@
+package items;
+
+import java.util.TreeMap;
+
+import interfaces.Container;
+import interfaces.Holdable;
+
+public class Bag extends StdItem implements Container { //wearable
+
+	protected TreeMap<String, Holdable> inventory = new TreeMap<String, Holdable>();	
+	
+	public Bag(ItemBuilder build) {
+		super(build);
+	}
+
+	@Override
+	public TreeMap<String, Holdable> getInventory() {
+		return new TreeMap<String, Holdable>(this.inventory);
+	}
+	
+	@Override
+	public String getName() {
+		return name;
+	}
+	
+	@Override
+	public int getId() {return id;}
+	
+	@Override
+	public void acceptItem(Holdable newItem) {
+		inventory.put(newItem.getName().toLowerCase() + newItem.getId(), newItem);
+	}	
+	
+	@Override
+	public void removeItemFromLocation(Holdable oldItem) {
+		if ((inventory.remove(oldItem.getName().toLowerCase() + oldItem.getId()) == null)) {
+			System.out.println("Failed to remove item from location: " + oldItem);
+		}
+	}
+	
+	@Override
+	public Holdable getHoldableFromString(String holdableString) {
+		holdableString = holdableString.toLowerCase();
+		String ceiling = inventory.ceilingKey(holdableString);
+		String floor = inventory.floorKey(holdableString);		
+		if (ceiling != null) {
+			if ((ceiling.equalsIgnoreCase(holdableString) || inventory.get(ceiling).getName().equalsIgnoreCase(holdableString))) {
+				return inventory.get(ceiling);
+			}
+		}
+		if (floor != null) {
+			if ((floor.equalsIgnoreCase(holdableString) || inventory.get(floor).getName().equalsIgnoreCase(holdableString))) {
+				return inventory.get(floor);
+			}
+		} 
+		return null;
+	}
+	
+	@Override
+	public boolean isEmpty() {
+		return true;
+	}
+}
+
